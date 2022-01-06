ed077b6e1d2142fdee63d78c59ef0b90a5d4eef2
Tony Dfouny <tony_dfouny@hotmail.com>
2022-01-06 22:55:47 +0200

    passport

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -42,31 +44,24 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-    if (!req.session.user) {
-       
-            var err = new Error('You are not authenticated!');                      
-            err.status = 401;
-            return next(err);
-            
-       
-    }
-    else {
-        if (req.session.user === 'authenticated') {
-            
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            next(err);
-        }
-    }
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
 }
 app.use(auth);
 
