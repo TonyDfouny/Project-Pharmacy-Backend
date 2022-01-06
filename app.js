ea46aa9339696249e70b2870059f521610d1fa15
Tony Dfouny <tony_dfouny@hotmail.com>
2022-01-06 21:55:54 +0200

    Express part 2

--- app.js
+++ app.js
@@ -42,40 +42,28 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth (req, res, next) {
     console.log(req.session);
 
     if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');                        
+       
+            var err = new Error('You are not authenticated!');                      
             err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
-            next(); // authorized
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+            return next(err);
+            
+       
     }
     else {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ',req.session);
+        if (req.session.user === 'authenticated') {
+            
             next();
         }
         else {
             var err = new Error('You are not authenticated!');
-            err.status = 401;
+            err.status = 403;
             next(err);
         }
     }
@@ -84,8 +72,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/products',productRouter);
 
 // catch 404 and forward to error handler
