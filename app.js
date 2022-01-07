6474f6e957746a4f35d5f48acafab18c186da38b
Tony <tony_dfouny@hotmail.com>
2022-01-08 01:28:06 +0200

    file upload

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var config = require('./config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/productRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -62,6 +63,7 @@ app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/products',productRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
