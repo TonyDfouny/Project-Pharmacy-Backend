224b13bc02af0e261516a262fe4c37c4bfd78a84
Tony <tony_dfouny@hotmail.com>
2022-01-08 21:51:19 +0200

    test finale 1

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -12,7 +12,7 @@ productRouter.use(bodyParser.json());
 productRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors,(req,res,next) => {
-    Products.find({})
+    Products.find(req.query)
     .then((products) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
